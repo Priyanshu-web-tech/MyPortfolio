817d4d3d172b19f5ec4025059895aadfa732abf1
Priyanshu Sharma <priyanshusharma6666@gmail.com>
2023-10-02 14:13:52 +0530

    Version-2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Customize manualChunks based on your project's dependencies
+          if (id.includes('node_modules/lodash')) {
+            return 'lodash';
+          }
+          // Add more conditions as needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust this value based on your needs
+  },
+});
